e2fac665d0e830f6e78e0172f9c2c75a0c872463
tanyavszabo <tatianavszabo@gmail.com>
2023-10-18 20:35:03 -0400

    class spa

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ router.hooks({
       case "Pizza":
         // New Axios get request utilizing already made environment variable
         axios
-          .get(`${process.env.PIZZA_PLACE_API_URL}/pizza`)
+          .get(`${process.env.PIZZA_PLACE_API_URL}/pizzas`)
           .then(response => {
             // We need to store the response to the state, in the next step but in the meantime let's see what it looks like so that we know what to store from the response.
             store.Pizza.pizzas = response.data;
